8dbe2bae0c29b4d26dd74982c44d2774fa0ede0a
yooseungju <seung338989@gmail.com>
2019-03-21 07:48:29 +0000

    04_customize form by formhelper

--- boards/forms.py
+++ boards/forms.py
@@ -1,20 +1,10 @@
 from django import forms
 from .models import Board
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
+from .models import Board
 
-# class BoardForm(forms.Form):
-#     title = forms.CharField(label='제목',widget=forms.TextInput(attrs={
-#                                                         'placeholder':'THE TITLE!',
-                                                        
-#     }))
-#     content = forms.CharField(label='내용', 
-#                                             error_messages={'required':'내용을 입력해 주세요'},
-#                                             widget=forms.Textarea(attrs={
-#                                                         'class':'Content-input',
-#                                                         'rows':5,
-#                                                         'cols':50,
-#                                                         'placeholder':'Fill the Content',
-                                                        
-#     }))
+
 
 class BoardForm(forms.ModelForm):
     class Meta:
@@ -23,11 +13,13 @@ class BoardForm(forms.ModelForm):
         widgets = {'title': forms.TextInput(attrs={
             'placeholder':'제목을 입력해 주세요',
             'class':'title',
+            
         }),
         'content':forms.TextInput(attrs={
             'placeholder':'내용을 입력해주세요',
             'class':'content',
-            'rows':5,'cols':50 })
+            'rows':5,
+            'cols':50 })
         
         }
         
@@ -35,6 +27,11 @@ class BoardForm(forms.ModelForm):
             'title':{'requird': '제목을 입력해주세요ㅜㅜ'},
             'content':{'requird': '내용을 입력해 주세요ㅜㅜ'}
         }
+    def __init__(self, *args , **kwargs):
+        super().__init__(*args , **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_method = "POST"
+        self.helper.add_input(Submit('submit','작성'))
         
         
     
