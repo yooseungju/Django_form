aad81fc5231f507f9b09dec95298456f3747bc2c
yooseungju <seung338989@gmail.com>
2019-04-09 01:42:28 +0000

    05_Authentication_01

--- myform/settings.py
+++ myform/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'accounts.apps.AccountsConfig',
     'boards.apps.BoardsConfig',
     'crispy_forms',
     'django.contrib.admin',
--- myform/urls.py
+++ myform/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 
 
 urlpatterns = [
+    path('accounts/', include('accounts.urls')),
     path('boards/', include('boards.urls')),
     path('admin/', admin.site.urls),
 ]
